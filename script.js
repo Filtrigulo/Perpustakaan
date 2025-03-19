167c3a40f803db39121e1045ba04457280647093
Filtrigulo <filtrig@gmail.com>
2025-03-19 14:00:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Perpustakaan Filtri Gulo</title>
-</head>
-<body>
-    <h1>Selamat Datang di Perpustakaan Filtri Gulo!</h1>
-    <p id="greeting"></p>
-
-    <script src="script.js"></script>
-</body>
-</html>
+document.getElementById('greeting').innerHTML = "Selamat datang di Perpustakaan Filtri Gulo";
